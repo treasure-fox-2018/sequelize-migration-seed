f8db1e00584638291d6f723f959b00cb3c79016b
Tushar <tusharbedi7373@hotmail.com>
2018-07-04 17:28:45 +0700

    completed release 1

---
+++ seeders/20180704074101-seed-contacts.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const fs = require("fs");
+let files = fs.readFileSync(__dirname + '/../contacts.csv','utf8').split('\n');
+let contacts = []
+for (let i = 0; i < files.length; i++) {
+  let file = files[i].split(',')
+  let obj = {
+    name: file[1],
+    email: file[2],
+    phone: file[3],
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
+  contacts.push(obj)
+}
+
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+   return queryInterface.bulkInsert('Contacts',contacts,{})
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+   return queryInterface.bulkDelete('Contacts', null, {})
+
+  }
+};
